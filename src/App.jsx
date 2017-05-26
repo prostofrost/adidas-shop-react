221afb81b77c03e6f98af9309859b4d54a749d58
prostofrost <fotofrost@gmail.com>
2017-05-26 19:16:15 +0300

    fix: add router redirect

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Redirect } from 'react-router-dom';
 
 import styled from 'styled-components';
 
@@ -25,7 +25,7 @@ export default () => (
     <Layout>
       <Sidebar />
       <Wrapper>
-        <Route exact path="/" component={List} />
+        <Redirect from="/" to="/products/football/shoes" />
         <Route exact path="/products/:section/:category" component={List} />
         <Route exact path="/products/:id" component={Show} />
       </Wrapper>
