5dadf9617b1c8c47b2746806335181525eb61850
prostofrost <fotofrost@gmail.com>
2017-05-31 12:59:22 +0300

    add deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,15 +1,4 @@
 #!/usr/bin/env bash
 
-npm i -g now
 now ./build -t "$NOW_TOKEN" -p
-
-# Grab deploy URL
-deploy_url=$(
-  now ./build -t "$NOW_TOKEN" -p ls build |
-  awk '/ url/,EOF' |
-  tail -n +2 |
-  awk '{ print $1 }'
-)
-
-echo "deploy finish, let's make alias"
-echo $deploy_url
+now ln
